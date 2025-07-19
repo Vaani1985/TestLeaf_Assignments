68a8ec6b04532b88d2fa4f987a4e4df7cc5c7732
Vaani1985 <freelanzor@gmail.com>
2025-07-19 19:34:18 +0530

    Week 1 1Day 1

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		/* Week 1 Day 1 --> Home Assignments
+		 * Create a class named Chrome with a main method. b) Inside the main method, declare and initialize the following variables:
+			1. version of type float with a value of 91.0f
+			2. developer of type String with a value of "Google"
+			3. isBeta of type boolean with a value of false
+			4. releaseYear of type int with a value of 2008
+			5. shortcutKey of type char with a value of 'C'*/
+		
+		double 	dollar 		= 91.0F;
+		String 	name 		= "Vaani";
+		boolean isBeta  	= false;
+		int 	releaseYear = 2008;
+		char 	shortcutKey = 'C';
+		
+		
+		System.out.println("The given double is 	: 	" + dollar);
+		System.out.println("The given String is 	: 	" + name);
+		System.out.println("The given boolean is 	: 	" + isBeta);
+		System.out.println("The given int is	: 	" + releaseYear);
+		System.out.println("The given char is	: 	" + shortcutKey);
+	}
+
+}
