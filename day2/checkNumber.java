e0f177e6f9db6fd1e18dd6cac2bc241e99ab45bc
Vaani1985 <freelanzor@gmail.com>
2025-07-20 18:38:28 +0530

    Week 1 Day 2

---
+++ day2/checkNumber.java
@@ -0,0 +1,16 @@
+package week1.day2;
+public class checkNumber 
+{
+	public static void main(String[] args) 
+	{
+		int num=10;
+		if (num > 0)
+		{
+			System.out.println("The given number " +num+ " is positive" );
+		}
+		else
+		{
+			System.out.println("The given number " +num+ " is negative");
+		}
+	}
+}
